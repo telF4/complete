07e43639fbe5176b2617587825362faea49f3f4f
yli13 <yli136347@gmail.com>
2022-12-29 11:01:13 +0800

    HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,46 @@
 public class HelloWorld {
+    public String name;
+    public String cardcode;
+    public long learncode;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCardcode(String cardcode) {
+        this.cardcode = cardcode;
+    }
+
+    public void setLearncode(long learncode) {
+        this.learncode = learncode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getLearncode() {
+        return learncode;
+    }
+
+    public String getCardcode() {
+        return cardcode;
+    }
+    public void display(){
+        System.out.println("name"+name);
+        System.out.println("learncode"+learncode);
+        System.out.println("cardcode"+cardcode);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello  World");//Hello World
+        HelloWorld show=new HelloWorld();
+        show.setCardcode("sdfsdfs");
+        show.setLearncode(54656);
+        show.setName("mike");
+    show.display();
     }
 }
+
+
+
+
